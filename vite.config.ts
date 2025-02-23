6f84d87cc16675c53af628600624c6ec93ac1fc9
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2025-02-23 19:12:28 +0900

    Vercel設定を更新し、ビルドコマンドをnpmに変更しました。また、tsconfig.jsonで型定義をnodeに変更し、vite.config.tsのエイリアスを修正しました。さらに、.gitignoreに.vercelを追加しました。

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@domain': path.resolve(__dirname, './src/domain'),
-      '@application': path.resolve(__dirname, './src/application'),
-      '@infrastructure': path.resolve(__dirname, './src/infrastructure'),
-      '@presentation': path.resolve(__dirname, './src/presentation'),
-      '@contexts': path.resolve(__dirname, './src/contexts'),
-      '@constants': path.resolve(__dirname, './src/constants'),
-      '@components': path.resolve(__dirname, './src/components')
+      '@': '/src',
+      '@domain': '/src/domain',
+      '@application': '/src/application',
+      '@infrastructure': '/src/infrastructure',
+      '@presentation': '/src/presentation',
+      '@contexts': '/src/contexts',
+      '@constants': '/src/constants',
+      '@components': '/src/components'
     }
   }
 }); 
